744512847fd511147ad2ff9d70e5f94fe68771e5
yerin040821 <166357095+yerin040821@users.noreply.github.com>
2026-05-01 14:48:58 +0900

    [Passed] 반복 출력하기 (8ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+
+    for (int i=0; i<n; i++) printf("12345^&*()_\n");
+
+    return 0;
+}
