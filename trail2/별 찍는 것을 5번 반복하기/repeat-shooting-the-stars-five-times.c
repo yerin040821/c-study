b4ce603d6584df2ff5ea956f49303b8585a0f877
yerin040821 <166357095+yerin040821@users.noreply.github.com>
2026-05-01 14:47:24 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (8ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print() {
+    printf("**********\n");
+}
+
+int main() {
+    // Please write your code here.
+    for (int i = 0; i < 5; i++) {
+        print();
+    }
+    return 0;
+}
